3167d4a0876d3c6187089a5ccd4a9c7b588e3f34
季维庆 <xiaomaprincess@gmail/com>
2018-11-29 10:11:39 +0800

    添加Swagger包

--- CoreWebApi/CoreWebApi/Startup.cs
+++ CoreWebApi/CoreWebApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace CoreWebApi
 {
@@ -26,6 +27,21 @@ namespace CoreWebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            #region 注册Swagger服务
+            services.AddSwaggerGen(x =>
+            {
+                // 文档描述
+                x.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1.0",
+                    Title = "Core API",
+                    Description = "接口说明文档",
+                    TermsOfService = "None",
+                    Contact = new Contact { Name = "AllenJee", Email = "xiaomaprncess@gmail.com" }
+                });
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,11 +50,26 @@ namespace CoreWebApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                #region 将Swagger配置在开发环境，生产环境禁用
+                //app.UseSwagger();
+                //app.UseSwaggerUI(x =>
+                //{
+                //    x.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiHelp V1");
+                //});
+                #endregion
             }
             else
             {
                 app.UseHsts();
             }
+
+            #region Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(x =>
+            {
+                x.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiHelp V1");
+            });
+            #endregion
 
             app.UseHttpsRedirection();
             app.UseMvc();
