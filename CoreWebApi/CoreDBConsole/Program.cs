66e5d3b9573faa08b79a1b2e87bb1c52dfdb4971
xiaoMaPrincess <xiaoMaPrincess@gmail.com>
2019-05-06 22:37:20 +0800

    补充上次提交

--- CoreWebApi/CoreDBConsole/Program.cs
+++ CoreWebApi/CoreDBConsole/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using Core.Common.EFCore;
+using Core.Common.Helper;
+using Core.Model;
+using Microsoft.Extensions.Options;
+using System;
 
 namespace CoreDBConsole
 {
     class Program
     {
+        /// <summary>
+        ///  连接字符串
+        /// </summary>
+        public static string ConnectionString { get; set; }
+        public Program(IOptions<AppSettings> options)
+        {
+            ConnectionString = options.Value.Mysql.Value;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            EFContext context = new EFContext(ConnectionString, DBTypeEnum.Mysql);
+            context.Database.EnsureCreated();
+            Console.WriteLine("数据库已初始化完毕，请查看！");
+            Console.ReadKey();
         }
     }
 }
---
+++ CoreWebApi/CoreWebApi/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.IServices;
+using Core.Model.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/v1/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserServices userServices;
+        public UserController(IUserServices _userServices)
+        {
+            userServices = _userServices;
+        }
+
+        /// <summary>
+        /// 用户注册
+        /// </summary>
+        /// <param name="loginVM"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<LoginResult> UserRegister(LoginVM loginVM)
+        {
+            return await  userServices.UserRegister(loginVM);
+            //return await new Task<LoginResult>(new LoginResult{Reason="1"});
+        }
+    }
+}
