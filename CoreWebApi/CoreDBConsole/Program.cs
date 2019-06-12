10945ffce165a26df2d98a1af9081643e90f838b
xiaoMaPrincess <xiaoMaPrincess@gmail.com>
2019-06-12 22:35:19 +0800

    add gitignore

--- CoreWebApi/CoreDBConsole/Program.cs
+++ CoreWebApi/CoreDBConsole/Program.cs
@@ -3,25 +3,53 @@ using Core.Common.Helper;
 using Core.Model;
 using Microsoft.Extensions.Options;
 using System;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 
 namespace CoreDBConsole
 {
     class Program
     {
-        /// <summary>
-        ///  连接字符串
-        /// </summary>
-        public static string ConnectionString { get; set; }
-        public Program(IOptions<AppSettings> options)
-        {
-            ConnectionString = options.Value.Mysql.Value;
-        }
+      
         static void Main(string[] args)
         {
-            EFContext context = new EFContext(ConnectionString, DBTypeEnum.Mysql);
-            context.Database.EnsureCreated();
-            Console.WriteLine("数据库已初始化完毕，请查看！");
+
+            MailMessage mailMsg = new MailMessage();//实例化对象
+            mailMsg.From = new MailAddress("923974733@qq.com", "季某人");//源邮件地址和发件人
+            mailMsg.To.Add(new MailAddress("xiaomaprincess@gmail.com"));//收件人地址
+            mailMsg.Subject = "邮件发送测试";//发送邮件的标题
+            StringBuilder sb = new StringBuilder();
+            sb.Append("测试测试测试测试");
+            sb.Append("嘿嘿");
+            mailMsg.Body = sb.ToString();//发送邮件的内容
+            //指定smtp服务地址（根据发件人邮箱指定对应SMTP服务器地址）
+            SmtpClient client = new SmtpClient();//格式：smtp.126.com  smtp.164.com
+            // 服务器名称
+            client.Host = "smtp.qq.com";
+            //端口
+            client.Port = 587;
+            //启用加密
+            client.EnableSsl = true;
+            //通过用户名和密码验证发件人身份
+            client.Credentials = new NetworkCredential("923974733@qq.com", "lsvogcrxkziobddd");
+            //发送邮件
+            try
+            {
+                client.Send(mailMsg);
+            }
+            catch (SmtpException ex)
+            {
+
+            }
+            Console.WriteLine("邮件已发送，请注意查收！");
             Console.ReadKey();
+
+            //string connectionString = @"Server=localhost;Database=coreweb;User=root;Password=Princess;";
+            //EFContext context = new EFContext(connectionString, DBTypeEnum.Mysql);
+            //context.Database.EnsureCreated();
+            //Console.WriteLine("数据库已初始化完毕，请查看！");
+
         }
     }
 }
